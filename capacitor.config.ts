06a9b42ded597c90d67648262ec3c91eab1bff9e
Jador de Vries <jador@netsupport.nl>
2024-07-29 09:25:10 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'go-charge-map-modal-test',
+  webDir: 'www'
+};
+
+export default config;
